f39ac5ada4f59de2cfb8e7a8f784901b67fee894
lamms06 <s.lammers@belastingdienst.nl>
2021-12-07 12:56:18 +0100

    Updaten App class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,7 @@
-public class App {
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class App extends Application {
+    // leeg laten
 }
