f23cb70f95c66602af5c1867fa6500dd23354e01
P.Tournoij <pepijn808@gmail.com>
2021-12-10 09:52:53 +0100

    Bezorgwijze werkend, wachtwoord + salt > Password classe werkend met pre persist wat oude meuk wegegooid

--- src/main/java/nl/bdmarktplaats/domain/Persoon/Gebruiker.java
+++ src/main/java/nl/bdmarktplaats/domain/Persoon/Gebruiker.java
@@ -4,11 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import nl.bdmarktplaats.domain.myenums.Bezorgwijze;
 
 import javax.persistence.*;
 import javax.xml.bind.annotation.XmlRootElement;
-import java.util.Set;
 
 @Data
 @Builder
@@ -32,7 +30,7 @@ public class Gebruiker extends Persoon {
     @Embedded
     private Adres adres;
     @Embedded
-    private Bezorgwijzentest bezorgwijze;
+    private Bezorgwijze bezorgwijze;
 
 }
 
